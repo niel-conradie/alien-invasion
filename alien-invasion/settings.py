59a2f88df627a21b5da665e1ed57596762cd5d73
Niel Conradie <nielconradie93@gmail.com>
2022-07-14 15:53:06 +0200

    Update settings.py
    Piloting the Ship: Adjusting the Ship's Speed.

--- alien-invasion/settings.py
+++ alien-invasion/settings.py
@@ -7,3 +7,6 @@ class Settings:
         self.screen_width = 1280
         self.screen_height = 720
         self.bg_color = (230, 230, 230)
+
+        # Ship settings.
+        self.ship_speed = 1.5
