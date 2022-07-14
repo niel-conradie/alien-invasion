8ca275bc9445a8903f0c50abd5a09237a8203804
Niel Conradie <nielconradie93@gmail.com>
2022-07-14 15:22:09 +0200

    Update alien_invasion.py
    The _check_events() and _update_screen() Methods: The _update_screen() Method.

--- alien-invasion/alien_invasion.py
+++ alien-invasion/alien_invasion.py
@@ -27,12 +27,20 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self._update_screen()
 
     def _check_events(self):
         """Respond to key presses and mouse events."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
