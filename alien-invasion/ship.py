82f905e4c27d5691af1e4bf7777404b19c1f7de5
Niel Conradie <nielconradie93@gmail.com>
2022-07-14 15:57:15 +0200

    Update ship.py
    Piloting the Ship: Adjusting the Ship's Speed.

--- alien-invasion/ship.py
+++ alien-invasion/ship.py
@@ -7,6 +7,7 @@ class Ship:
     def __init__(self, game):
         """Initialize the ship and set it's starting position."""
         self.screen = game.screen
+        self.settings = game.settings
         self.screen_rect = game.screen.get_rect()
 
         # Load the ship image and get it's rect.
@@ -16,16 +17,23 @@ class Ship:
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
+
         # Moving flag.
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Update the ship's position based on the movement flag."""
+        # Update the ship's x value, not the rect.
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x.
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at it's current location."""
