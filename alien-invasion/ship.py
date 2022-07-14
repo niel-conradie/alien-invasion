036fa870f80bbb7505908d6b765b25a1c2bb6607
Niel Conradie <nielconradie93@gmail.com>
2022-07-14 15:08:20 +0200

    Create ship.py
    Adding the Ship Image: Creating the Ship Class.

---
+++ alien-invasion/ship.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+class Ship:
+    """A class to manage the ship."""
+
+    def __init__(self, game):
+        """Initialize the ship and set it's starting position."""
+        self.screen = game.screen
+        self.screen_rect = game.screen.get_rect()
+
+        # Load the ship image and get it's rect.
+        self.image = pygame.image.load("alien-invasion/assets/ship.bmp")
+        self.rect = self.image.get_rect()
+
+        # Start each new ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """Draw the ship at it's current location."""
+        self.screen.blit(self.image, self.rect)
