85b731e9a0b7146d00de9e97bc2f9ae29fb81edb
Niel Conradie <nielconradie93@gmail.com>
2022-07-15 12:24:53 +0200

    Update settings.py
    Shooting Bullets: Limiting thhe Number of Bullets.

--- alien-invasion/settings.py
+++ alien-invasion/settings.py
@@ -12,6 +12,7 @@ class Settings:
         self.ship_speed = 1.5
 
         # Bullet settings.
+        self.bullets_allowed = 3
         self.bullet_speed = 1.0
         self.bullet_width = 3
         self.bullet_height = 15
