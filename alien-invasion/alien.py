e3a85f6ae3b26b5d5e69ea51341657d05adc69d2
Niel Conradie <nielconradie93@gmail.com>
2022-07-15 14:08:55 +0200

    Update alien.py
    Making the Fleet Move: Moving the Aliens Right.

--- alien-invasion/alien.py
+++ alien-invasion/alien.py
@@ -10,6 +10,7 @@ class Alien(Sprite):
         """Initialize the alien and set it's starting position."""
         super().__init__()
         self.screen = game.screen
+        self.settings = game.settings
 
         # Load the alien image and set its rect attribute.
         self.image = pygame.image.load("alien-invasion/assets/alien.bmp")
@@ -21,3 +22,8 @@ class Alien(Sprite):
 
         # Store the alien's exact horizontal position.
         self.x = float(self.rect.x)
+
+    def update(self):
+        """Move the alien to the right."""
+        self.x += self.settings.alien_speed
+        self.rect.x = self.x
