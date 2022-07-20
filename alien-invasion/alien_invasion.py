76ebdb97c90217938098b7c23e2e632c4c083509
Niel Conradie <nielconradie93@gmail.com>
2022-07-20 15:21:21 +0200

    Update alien_invasion.py
    Scoring: Updating the Score as Aliens Are Shot Down.

--- alien-invasion/alien_invasion.py
+++ alien-invasion/alien_invasion.py
@@ -113,12 +113,16 @@ class AlienInvasion:
     def _check_bullet_alien_collisions(self):
         """Respond to bullets-aliens collisions."""
         # Remove any bullets and aliens that have collided.
-        collections = pygame.sprite.groupcollide(
+        collisions = pygame.sprite.groupcollide(
             self.bullets,
             self.aliens,
             True,
             True,
         )
+
+        if collisions:
+            self.stats.score += self.settings.alien_points
+            self.scoreboard.prepare_score()
 
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
