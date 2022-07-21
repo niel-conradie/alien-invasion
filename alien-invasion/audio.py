f241e9b9e92a6d0d6ac6f5a766705376ea9eae38
Niel Conradie <nielconradie93@gmail.com>
2022-07-21 12:18:03 +0200

    Create audio.py
    Audio: Loading in audio assets.

---
+++ alien-invasion/audio.py
@@ -0,0 +1,9 @@
+import pygame
+
+
+# Initialize pygame mixer.
+pygame.mixer.init()
+
+# Loading audio files into variables.
+explosion = pygame.mixer.Sound("alien_invasion/assets/sfx_explosion.wav")
+laser = pygame.mixer.Sound("alien_invasion/assets/sfx_laser.wav")
