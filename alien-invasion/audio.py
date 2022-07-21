e9f5c1888529afc49239b31560b33b511def8e3e
Niel Conradie <nielconradie93@gmail.com>
2022-07-21 12:27:26 +0200

    Update audio.py
    Bug: Fixed loading assets from unkown location.

--- alien-invasion/audio.py
+++ alien-invasion/audio.py
@@ -5,5 +5,5 @@ import pygame
 pygame.mixer.init()
 
 # Loading audio files into variables.
-explosion = pygame.mixer.Sound("alien_invasion/assets/sfx_explosion.wav")
-laser = pygame.mixer.Sound("alien_invasion/assets/sfx_laser.wav")
+explosion = pygame.mixer.Sound("alien-invasion/assets/sfx_explosion.wav")
+laser = pygame.mixer.Sound("alien-invasion/assets/sfx_laser.wav")
