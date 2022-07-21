1449d1257384e72211d83c3bd83ccfa02508101d
Niel Conradie <nielconradie93@gmail.com>
2022-07-21 12:07:06 +0200

    Create run.py
    Refactoring: Create dedicated run file for ease of use.

---
+++ alien-invasion/run.py
@@ -0,0 +1,13 @@
+from alien_invasion import AlienInvasion
+
+
+def run():
+    """Alien-Invasion"""
+    run = AlienInvasion()
+
+    # Starting the game.
+    run.start_game()
+
+
+if __name__ == "__main__":
+    run()
