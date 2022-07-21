42e1ad8d7f14b1a12301443b1ee60808f97ff424
Niel Conradie <nielconradie93@gmail.com>
2022-07-21 11:49:08 +0200

    Update ship.py
    Scoring: Displaying the Number of Ships.

--- alien-invasion/ship.py
+++ alien-invasion/ship.py
@@ -1,11 +1,14 @@
 import pygame
 
+from pygame.sprite import Sprite
 
-class Ship:
+
+class Ship(Sprite):
     """A class to manage the ship."""
 
     def __init__(self, game):
         """Initialize the ship and set it's starting position."""
+        super().__init__()
         self.screen = game.screen
         self.settings = game.settings
         self.screen_rect = game.screen.get_rect()
