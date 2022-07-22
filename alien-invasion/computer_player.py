72d66fa1d6aa3823a199efe45d0aebc32386fca0
Niel Conradie <nielconradie93@gmail.com>
2022-07-22 13:23:07 +0200

    Create computer_player.py
    Computer Player: Firing automatically.

---
+++ alien-invasion/computer_player.py
@@ -0,0 +1,38 @@
+import pygame
+
+from alien_invasion import AlienInvasion
+
+
+class ComputerPlayer:
+    """Computer player for Alien Invasion."""
+
+    def __init__(self, game):
+        """Initialize class attributes."""
+        self.game = game
+
+    def start_game(self):
+        """Replaces the original run_game(), so we can interject our own controls."""
+        # Start out in an active state, and hide the mouse.
+        self.game.stats.game_active = True
+        pygame.mouse.set_visible(False)
+
+        # Start the main loop for the game.
+        while True:
+            # Still call game._check_events(), so we can use keyboard to quit.
+            self.game._check_events()
+
+            if self.game.stats.game_active:
+                self.game.ship.update()
+                self.game._update_bullets()
+                self.game._update_aliens()
+                self.game._fire_bullet()
+
+            self.game._update_screen()
+
+
+if __name__ == "__main__":
+    run = AlienInvasion()
+
+    # Starting the game.
+    player = ComputerPlayer(run)
+    player.start_game()
