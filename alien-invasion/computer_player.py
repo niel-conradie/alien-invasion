4d8617b11f06c0a371496a39f6d6214eabefd379
Niel Conradie <nielconradie93@gmail.com>
2022-07-29 13:55:49 +0200

    Update computer_player.py
    Computer PLayer: Moving ship right and left.

--- alien-invasion/computer_player.py
+++ alien-invasion/computer_player.py
@@ -19,6 +19,21 @@ class ComputerPlayer:
             # Still call game._check_events(), so we can use keyboard to quit.
             self.game._check_events()
 
+            # Sweep the ship right and left continuously.
+            ship = self.game.ship
+            screen_rect = self.game.screen.get_rect()
+
+            if not ship.moving_right and not ship.moving_left:
+                # Ship hasn't started moving yet; move to the right.
+                ship.moving_right = True
+            elif ship.moving_right and ship.rect.right > screen_rect.right - 10:
+                # Ship about to hit right edge; move left.
+                ship.moving_right = False
+                ship.moving_left = True
+            elif ship.moving_left and ship.rect.left < 10:
+                ship.moving_left = False
+                ship.moving_right = True
+
             if self.game.stats.game_active:
                 self.game.ship.update()
                 self.game._update_bullets()
